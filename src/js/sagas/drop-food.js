42b89ff6d8e17bac097fdd492c75d68c451439f1
Niels Vleeming <niels.vleeming@greenhousegroup.com>
2016-11-22 23:55:08 +0100

    No need to fork here
    (copy paste derp)

--- src/js/sagas/drop-food.js
+++ src/js/sagas/drop-food.js
@@ -1,13 +1,11 @@
-import { fork, take, put } from 'redux-saga/effects';
+import { take, put } from 'redux-saga/effects';
 
 import actionTypes from 'constants/action-types';
 import dropFood from 'actions/drop-food';
 
 export default function* () {
-  yield fork(function* () {
-    while (true) {
-      yield put(dropFood());
-      yield take(actionTypes.SNAKE_EATS);
-    }
-  });
+  while (true) {
+    yield put(dropFood());
+    yield take(actionTypes.SNAKE_EATS);
+  }
 }
