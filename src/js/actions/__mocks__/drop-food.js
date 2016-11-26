1c86c1b616368cc9b104d631ee9caebe1f27300a
Niels Vleeming <niels.vleeming@greenhousegroup.com>
2016-11-26 21:33:30 +0100

    Remove commented code from mock

--- src/js/actions/__mocks__/drop-food.js
+++ src/js/actions/__mocks__/drop-food.js
@@ -5,13 +5,3 @@ export default () => ({
     y: 321
   }
 });
-//
-// export default function() {
-//   return {
-//     type: 'MOCK_DROP_FOOD',
-//     payload: {
-//       x: 123,
-//       y: 321
-//     }
-//   };
-// };
